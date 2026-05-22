5d2a994ee9334ac71396518516a55b6ff75a2aa9
Aayash <dangaayash@gmail.com>
2026-05-22 15:30:18 +0545

    zustand auth store

---
+++ src/store/authStore.js
@@ -0,0 +1,25 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+
+      setAuth: (user, token) => set({ user, token }),
+
+      updateUser: (updates) =>
+        set((state) => ({ user: { ...state.user, ...updates } })),
+
+      logout: () => set({ user: null, token: null }),
+    }),
+    {
+      name: 'lms-auth',           
+      partialize: (state) => ({   
+        user: state.user,
+        token: state.token,
+      }),
+    }
+  )
+)
