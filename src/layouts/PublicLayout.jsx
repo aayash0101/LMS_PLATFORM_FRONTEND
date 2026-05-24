940a2c8c2e22ae8d1c066014a8c81035862cafc3
Aayash <dangaayash@gmail.com>
2026-05-24 10:27:50 +0545

    updated public layout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '@/components/Navbar'
 
 const PublicLayout = () => (
   <div className="min-h-screen flex flex-col">
-    {/* Navbar goes here in F3 */}
+    <Navbar />
     <main className="flex-1">
       <Outlet />
     </main>
-    {/* Footer goes here in F3 */}
+    <footer className="border-t py-6 text-center text-sm text-muted-foreground">
+      © {new Date().getFullYear()} LearnHub. All rights reserved.
+    </footer>
   </div>
 )
 
