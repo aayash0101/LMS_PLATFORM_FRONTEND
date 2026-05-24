cf306d0a78ce511d00971cdf1e814a68db0cf1fb
Aayash <dangaayash@gmail.com>
2026-05-24 10:59:20 +0545

    use enroll

---
+++ src/features/enrollments/hooks/useEnroll.js
@@ -0,0 +1,15 @@
+// src/features/enrollments/hooks/useEnroll.js
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { enrollInCourseApi } from '../api/enrollments.api'
+
+export const useEnroll = (courseId) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: () => enrollInCourseApi(courseId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['enrollments'] })
+      queryClient.invalidateQueries({ queryKey: ['enrollment', courseId] })
+    },
+  })
+}
