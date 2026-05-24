8a07c305782671b1252a2a8a5f834d1523501c31
Aayash <dangaayash@gmail.com>
2026-05-24 11:40:22 +0545

    use update course hook

---
+++ src/features/courses/hooks/useUpdateCourse.js
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { updateCourseApi, publishCourseApi } from '../api/instructor.courses.api'
+
+export const useUpdateCourse = (id) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (data) => updateCourseApi(id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['instructor-courses'] })
+      queryClient.invalidateQueries({ queryKey: ['course'] })
+    },
+  })
+}
+
+export const usePublishCourse = (id) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: () => publishCourseApi(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['instructor-courses'] })
+    },
+  })
+}
