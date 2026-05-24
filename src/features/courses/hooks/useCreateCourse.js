4e09f5ce1b6a687f8209477e9a3da3226fee1702
Aayash <dangaayash@gmail.com>
2026-05-24 11:39:39 +0545

    use create course hook

---
+++ src/features/courses/hooks/useCreateCourse.js
@@ -0,0 +1,16 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+import { createCourseApi } from '../api/instructor.courses.api'
+
+export const useCreateCourse = () => {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  return useMutation({
+    mutationFn: createCourseApi,
+    onSuccess: ({ data }) => {
+      queryClient.invalidateQueries({ queryKey: ['instructor-courses'] })
+      navigate(`/instructor/courses/${data.data._id}/edit`)
+    },
+  })
+}
