b18e5d2b3333a68c6b339c55e810308105a1fbc9
Aayash <dangaayash@gmail.com>
2026-05-24 10:45:21 +0545

    Student layout updated

--- src/layouts/StudentLayout.jsx
+++ src/layouts/StudentLayout.jsx
@@ -1,11 +1,17 @@
+// src/layouts/StudentLayout.jsx
 import { Outlet } from 'react-router-dom'
+import Navbar from '@/components/Navbar'
+import StudentSidebar from '@/components/StudentSidebar'
 
 const StudentLayout = () => (
-  <div className="min-h-screen flex">
-    {/* Student sidebar goes here in F4 */}
-    <main className="flex-1 p-6">
-      <Outlet />
-    </main>
+  <div className="min-h-screen flex flex-col">
+    <Navbar />
+    <div className="flex flex-1">
+      <StudentSidebar />
+      <main className="flex-1 p-8 bg-background overflow-auto">
+        <Outlet />
+      </main>
+    </div>
   </div>
 )
 
