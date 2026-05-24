1ce10ef0b4c95c3d1b02165b106c336de652b154
Aayash <dangaayash@gmail.com>
2026-05-24 10:01:46 +0545

    auth api

---
+++ src/features/auth/api/auth.api.js
@@ -0,0 +1,13 @@
+import api from '@/api/axios'
+
+export const registerApi = (data) =>
+  api.post('/auth/register', data)
+
+export const loginApi = (data) =>
+  api.post('/auth/login', data)
+
+export const logoutApi = () =>
+  api.post('/auth/logout')
+
+export const getMeApi = () =>
+  api.get('/auth/me')
