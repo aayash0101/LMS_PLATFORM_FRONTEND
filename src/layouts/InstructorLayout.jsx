6bc4785545520178f4fc6e46d1763ddf5e3e0005
Aayash <dangaayash@gmail.com>
2026-05-24 11:43:39 +0545

    Instructor layout updated

--- src/layouts/InstructorLayout.jsx
+++ src/layouts/InstructorLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '@/components/Navbar'
+import InstructorSidebar from '@/components/InstructorSidebar'
 
 const InstructorLayout = () => (
-  <div className="min-h-screen flex">
-    {/* Instructor sidebar goes here in F5 */}
-    <main className="flex-1 p-6">
-      <Outlet />
-    </main>
+  <div className="min-h-screen flex flex-col">
+    <Navbar />
+    <div className="flex flex-1">
+      <InstructorSidebar />
+      <main className="flex-1 p-8 bg-background overflow-auto">
+        <Outlet />
+      </main>
+    </div>
   </div>
 )
 
